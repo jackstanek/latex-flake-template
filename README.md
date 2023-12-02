b457c12ab30174bb8fb285d7392f4f7aa0d17943
Jack Stanek <stanekjack@gmail.com>
2023-12-01 22:52:27 -0600

    Add README, correctly structure template

---
+++ README.md
@@ -0,0 +1,8 @@
+# LaTeX Flake Template
+
+This repository is a flake template for writing LaTeX documents. To use, simply
+run the following:
+
+```
+nix flake init -t github:jackstanek/latex-flake-template
+```
---
+++ latex/flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "LaTeX document";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        docname = "doc";
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit
+            (pkgs.texlive)
+            scheme-small
+            latex-bin
+            latexmk
+            # Other packages here
+          ;
+        };
+        deps = with pkgs; [ tex coreutils just bash ];
+      in rec {
+        devShells.default = pkgs.mkShell {
+          buildInputs = deps;
+          shellHook = ''
+            export DOCNAME=${docname}
+          '';
+        };
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = doc;
+            src = self;
+            buildInputs = deps;
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            buildPhase = ''
+              export DOCNAME="${docname}"
+              export PATH="${pkgs.lib.makeBinPath buildInputs}"
+              just build
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp ${docname}.pdf $out/
+            '';
+          };
+        };
+        packages.default = packages.document;
+      });
+}
